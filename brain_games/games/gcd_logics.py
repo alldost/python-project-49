fa4492d3d4b71ec045c7c0ac1871fcd7785e8396
ezhov_dv <alldost@bk.ru>
2024-03-10 21:55:02 +0500

    resolve gcd failure

--- brain_games/games/gcd_logics.py
+++ brain_games/games/gcd_logics.py
@@ -5,7 +5,7 @@ def greatest_gcd():
     ''' Генерация и вывод чисел, определение их НОД '''
     first_number = random.randint(1, 100)
     second_number = random.randint(1, 100)
-    print(f'Question: {first_number}  {second_number}')
+    print(f'Question: {first_number} {second_number}')
     gcd = first_number
     while gcd >= 1:
         if first_number % gcd == 0 and second_number % gcd == 0:
